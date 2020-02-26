97fc14f5685dcf5257f151bcdc883a4626fd38fa
Enrique De Diego <enrique@catharsis.io>
2020-02-26 18:33:14 -0500

    Crap I need a logo..hello google fonts

---
+++ tests/acceptance/grocer-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | grocer', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to Grocer!');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/lists');
+  });
+});
